95a533f0cc36693b1b2141595e32a4b7b8150e43
Chemax911 <cherviakov.max@gmail.com>
2020-11-28 21:25:59 +0200

    Add model Category

--- www/catalog/apps.py
+++ www/catalog/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+    verbose_name = 'Каталог'
+    verbose_name_plural = 'Каталоги'
--- www/catalog/models.py
+++ www/catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """МОДЕЛЬ КАТЕГОРИЙ"""
+    name = models.CharField('Название Категории', max_length=100)
+    slug = models.SlugField('url', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
