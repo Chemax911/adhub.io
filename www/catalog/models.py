30acd2a3ac301112398378ba85b1c3bc3bf9c608
Chemax911 <cherviakov.max@gmail.com>
2020-11-28 21:51:18 +0200

    Add model Category

--- www/catalog/models.py
+++ www/catalog/models.py
@@ -3,12 +3,29 @@ from django.db import models
 
 class Category(models.Model):
     """МОДЕЛЬ КАТЕГОРИЙ"""
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     name = models.CharField('Название Категории', max_length=100)
     slug = models.SlugField('url', max_length=100)
 
     def __str__(self):
         return self.name
 
+
+class Condition(models.Model):
+    """МОДЕЛЬ СОСТОЯНИЯ"""
+
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        ordering = ['name']
+        verbose_name = 'Состояние'
+        verbose_name_plural = 'Состояния'
+
+    name = models.CharField('Пазвание', max_length=100)
+    description = models.TextField('Краткое описание')
+
+    def __str__(self):
+        return self.name
